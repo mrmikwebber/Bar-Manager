14b23e661df58cbd4ef50fc5401eebc6c42cb1d0
mrmikwebber <mi.webber.ii@gmail.com>
2026-09-25 12:09:51 -0500

    feat: add MVP foundation

---
+++ src/app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Bar Manager",description:"Cocktail R&D workbench"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="en"><body>{children}</body></html>}
